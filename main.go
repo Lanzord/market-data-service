6218cad02f112a9c48b9eed3345f017d071df0bd
Lanzord <181258594+Lanzord@users.noreply.github.com>
2026-06-06 18:16:46 +0800

    feat(http): add GET /ping endpoint

--- main.go
+++ main.go
@@ -9,5 +9,8 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello from my market data service")
 	})
+	http.HandleFunc("GET /ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "pong")
+	})
 	http.ListenAndServe(":8080", nil)
 }
